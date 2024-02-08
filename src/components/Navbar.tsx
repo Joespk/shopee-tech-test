99245979048dfa62924a887ac1d5bd7c08d32c58
Joespk <supakritjoe@gmail.com>
2024-02-08 19:44:00 +0700

    add top nav left

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,68 @@
-import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/shopeelogo.png";
 import cartlogo from "@/public/shoppingcart.svg";
 import seachicon from "@/public/seachicon.svg";
+import facebook from "@/public/logo/facebook.svg";
+import instragam from "@/public/logo/instragm.svg";
+import line from "@/public/logo/line.svg";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gradient-to-t from-[#ff7337] to-[#ee4d2d] z-50 h-20">
-      <div className="w-[1200px] mx-auto">
-        <div className="flex flex-row items-center justify-between gap-16 ">
-          <Image src={logo} alt="logo" width={150} />
-          <div className="container flex flex-col">
-            <div className="p-1 shadow-md container bg-white rounded-lg flex flex-row items-center justify-between text-black ">
-              <input
-                type="text"
-                placeholder="ลูกค้าใหม่ รับเลย โค้ดส่วนลด หรือโค้ดส่งฟรี"
-                className="container px-2"
-              />
-              <button className="bg-[#ff7337] rounded-lg min-w-16 flex justify-center h-[34px] items-center px-3.5">
-                <Image src={seachicon} alt="search" width={16} />
-              </button>
+    <header className="bg-gradient-to-t from-[#ff7337] to-[#ee4d2d] z-50 h-[120px] sticky top-0 flex flex-col">
+      <div className="h-1 w-[1200px] mx-auto my-1 flex justify-around">
+        <nav className="m-auto flex justify-between">
+          <div className="flex justify-between items-center gap-3 text-white text-sm">
+            <p className="border-r-2 border-white pr-2">Seller Centre</p>
+            <p className="border-r-2 border-white pr-2">เริ่มต้นขายสินค้า</p>
+            <p>ดาวน์โหลด</p>
+            <div className="flex justify-between gap-1 items-center border-l-2 border-white pl-2">
+              <p>ติดตามเราบน</p>
+              <div className="flex gap-2">
+                <Image src={facebook} alt="facebook" width={20} />
+                <Image src={instragam} alt="instragam" width={20} />
+                <Image src={line} alt="line" width={20} />
+              </div>
             </div>
-            <div></div>
           </div>
-
-          <Image src={cartlogo} alt="cart" width={26} />
-        </div>
+        </nav>
       </div>
-    </nav>
+      <nav>
+        <div className="w-[1200px] mx-auto flex justify-around my-2">
+          <div className="flex flex-row items-center justify-between gap-16 ">
+            <Image src={logo} alt="logo" width={150} />
+            <div className="container flex flex-col py-3">
+              <div className="p-1 shadow-md container bg-white rounded-lg flex flex-row items-center justify-between text-black ">
+                <input
+                  type="text"
+                  placeholder="ลูกค้าใหม่ รับเลย โค้ดส่วนลด หรือโค้ดส่งฟรี"
+                  className="container px-2"
+                />
+                <button className="bg-[#ff7337] rounded-lg min-w-16 flex justify-center h-[34px] items-center px-3.5 ">
+                  <Image src={seachicon} alt="search" width={16} />
+                </button>
+              </div>
+              <div>
+                <ul className="flex flex-row gap-5 items-center py-2 text-xs text-white">
+                  <li>สมาชิกใหม่</li>
+                  <li>เก้าอี้ 1 บาท</li>
+                  <li>เคส</li>
+                  <li>เสื้อครอปเท่ๆ</li>
+                  <li>สายชาร์จเร็ว</li>
+                  <li>ฟรีลูกค้าใหม่</li>
+                  <li>หูฟัง 1 บาท</li>
+                  <li>แจกฟรี</li>
+                  <li>เสื้อกันหนาวมีซิป</li>
+                  <li>ลูกค้าใหม่</li>
+                  <li>เครื่องใช้ไฟฟ้าหม้ออบ</li>
+                </ul>
+              </div>
+            </div>
+
+            <Image src={cartlogo} alt="cart" width={26} />
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
