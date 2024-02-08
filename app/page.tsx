31c8df37af517c9ce913c5fc3b1a539d826aa0b3
Joespk <supakritjoe@gmail.com>
2024-02-08 23:14:00 +0700

    simple card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import Simplecard from "@/src/components/card/Simplecard";
+import { fetchProduct } from "./action/fetchproduct";
+import { ProductDTO } from "@/src/type/dto";
 
-export default function Home() {
-  return <main></main>;
+export default async function Home() {
+  const productlist = await fetchProduct();
+  const products = productlist.products;
+
+  return (
+    <main>
+      <section>
+        <div className="container flex justify-around">
+          <div className="grid grid-cols-6 gap-2 mt-3">
+            {products?.map((product: ProductDTO) => (
+              <Simplecard key={product.id} Product={product} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
