f09f8b5e9d451fdf00cfa9ed1b4400f1c60c3244
Joespk <supakritjoe@gmail.com>
2024-02-09 11:18:48 +0700

    add flashsell

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Simplecard from "@/src/components/card/Simplecard";
 import { fetchProduct } from "./action/fetchproduct";
 import { ProductDTO } from "@/src/type/dto";
+import React from "react";
 import Flashsellcard from "@/src/components/card/Flashsellcard";
+import Image from "next/image";
+import flashsell from "@/public/flashselltime.png";
 
 export default async function Home() {
   const productlist = await fetchProduct();
@@ -11,25 +14,21 @@ export default async function Home() {
     (product: ProductDTO) => product.discountPercentage >= 15
   );
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
-
   return (
     <main>
       <section>
-        <div className="container flex justify-around">
-          <Slider {...settings}>
-            {FlashProducts?.map((product: ProductDTO) => (
-              <div key={product.id}>
-                <Flashsellcard Product={product} />
-              </div>
+        <div className="container flex flex-col items-center mx-auto py-8 md:py-24 relative">
+          <Image
+            src={flashsell}
+            alt="flashsell"
+            width={150}
+            className="absolute top-14 left-[12.5%]"
+          />
+          <div className="grid grid-cols-6 gap-2 mt-3 justify-around">
+            {FlashProducts?.slice(0, 6).map((product: ProductDTO) => (
+              <Flashsellcard key={product.id} Product={product} />
             ))}
-          </Slider>
+          </div>
         </div>
       </section>
       <section>
